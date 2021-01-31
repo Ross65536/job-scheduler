b31625dacc68a9013d56a222ede15b5faee22bf1
Rostyslav Khoptiy <rk65536@protonmail.com>
2021-01-31 15:35:28 +0100

    syncronize users jobs code

--- server/internal/user.go
+++ server/internal/user.go
@@ -3,25 +3,31 @@ package internal
 import (
 	"crypto/subtle"
 	"log"
+	"sync"
 
 	"github.com/google/uuid"
 )
 
 type User struct {
-	token string         // the API token given to the user to access the API, will be generated using a CSPRNG, stored in hex or base64 format
-	jobs  map[string]Job // Index. list of jobs that belong to the user. Index key is the job ID.
+	token    string         // the API token given to the user to access the API, will be generated using a CSPRNG, stored in hex or base64 format
+	jobs     map[string]Job // Index. list of jobs that belong to the user. Index key is the job ID.
+	jobsLock sync.Mutex
 	// Username string, // not necessary, already stored in the index
 	// Password string, // not used, would be stored as hash using BCrypt
 }
 
+// there is no need to synchronize access with a mutex to this index,
+// since it is never modified by the backend, only pre-initialized
 var usersIndex map[string]User // maps username to user struct
 
 func (u *User) GetAllJobs() []Job {
+	u.jobsLock.Lock()
 	jobsList := make([]Job, 0, len(u.jobs))
 
 	for _, value := range u.jobs {
 		jobsList = append(jobsList, value)
 	}
+	u.jobsLock.Unlock()
 
 	return jobsList
 }
@@ -40,10 +46,12 @@ func generateNewID(keys map[string]Job) string {
 }
 
 func (u *User) CreateJob(command []string, pid int) *Job {
+	u.jobsLock.Lock()
 	id := generateNewID(u.jobs)
 
 	job := MakeJob(id, command, pid)
 	u.jobs[id] = job
+	u.jobsLock.Unlock()
 
 	return &job
 }
