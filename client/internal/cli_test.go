d2b2e313168f9f792e96b9456a7ca84a5e779edf
Rostyslav Khoptiy <rk65536@protonmail.com>
2021-02-04 18:06:31 +0100

    add client functional test for show

---
+++ client/internal/cli_test.go
@@ -0,0 +1,103 @@
+package internal_test
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/ros-k/job-manager/client/internal"
+)
+
+func assertNotError(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func assertContains(t *testing.T, actual, substr string) {
+	if !strings.Contains(actual, substr) {
+		t.Fatalf("String %s doesn't contain %s", actual, substr)
+	}
+}
+
+func assertEquals(t *testing.T, actual interface{}, expected interface{}) {
+	if actual != expected {
+		t.Fatalf("Invalid field, expected %s, was %s", expected, actual)
+	}
+}
+
+func setupTestServer(t *testing.T, returnJson []byte, expectedMethod, expectedUriPath string, expectedJsonBits []string) *httptest.Server {
+	const jsonMime = "application/json"
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertEquals(t, expectedUriPath, r.URL.Path)
+		assertEquals(t, expectedMethod, r.Method)
+
+		// assert.Contains(t, r.Header.Get("Authorization"), "Bearer")
+		// assert.Contains(t, r.Header.Get("Authorization"), testApiKey)
+
+		if returnJson != nil {
+			w.Header().Set("Content-Type", jsonMime)
+			w.Write(returnJson)
+		}
+	})
+
+	return httptest.NewServer(handler)
+}
+
+func captureOutput(t *testing.T, f func()) string {
+	rescueStdout := os.Stdout
+	r, w, err := os.Pipe()
+	assertNotError(t, err)
+
+	os.Stdout = w
+
+	f()
+
+	w.Close()
+	out, err := ioutil.ReadAll(r)
+	assertNotError(t, err)
+	os.Stdout = rescueStdout
+
+	return string(out)
+}
+
+func TestCanShowJob(t *testing.T) {
+	id := "123XYZ902"
+	job := internal.JobViewFull{
+		JobViewPartial: internal.JobViewPartial{
+			JobViewCommand: internal.JobViewCommand{
+				Command: []string{"ls", "-l", "/"},
+			},
+			ID:        id,
+			Status:    "RUNNING",
+			CreatedAt: time.Now(),
+		},
+		Stdout: "STDOUT123",
+		Stderr: "STDERR456",
+	}
+
+	requestJson, err := json.Marshal(job)
+	assertNotError(t, err)
+
+	server := setupTestServer(t, requestJson, "GET", "/api/jobs/"+id, nil)
+	defer server.Close()
+
+	uri, err := url.ParseRequestURI(server.URL)
+	assertNotError(t, err)
+
+	output := captureOutput(t, func() {
+		err = internal.Start([]string{"client", "show", id, "-c=http://user:pass@" + uri.Host})
+		assertNotError(t, err)
+	})
+
+	assertContains(t, output, "RUNNING")
+	assertContains(t, output, "STDOUT123")
+	assertContains(t, output, "STDERR456")
+}
